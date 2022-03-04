f1db07eb6438a1dd7d4ef0b01143448f66d1de6a
hsw1920 <hsw1920@gmail.com>
2022-03-05 00:37:50 +0900

    remove .git files

---
+++ ios_learning/development/Clima-iOS13/Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        searchTextField.delegate = self
+    }
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        searchTextField.endEditing(true)// 키보드 닫힘
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)// 키보드 닫힘
+        return true
+    }
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //이 함수가 없다면 위에서 2번 반복해야함
+    // 유용한 대리자 메서드임
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        //Use searchTextField.text to get weather for that city
+        
+        searchTextField.text = ""
+    }
+    
+}
+
