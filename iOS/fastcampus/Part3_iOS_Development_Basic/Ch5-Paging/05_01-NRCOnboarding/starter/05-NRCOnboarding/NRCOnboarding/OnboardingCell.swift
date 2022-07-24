271a3987a9ae228e58f1c2b4799595ba038f72dc
hsw1920 <hsw1920@gmail.com>
2022-07-24 17:44:18 +0900

    20220724

---
+++ iOS/fastcampus/Part3_iOS_Development_Basic/Ch5-Paging/05_01-NRCOnboarding/starter/05-NRCOnboarding/NRCOnboarding/OnboardingCell.swift
@@ -0,0 +1,23 @@
+//
+//  OnboadingCell.swift
+//  NRCOnboarding
+//
+//  Created by 홍승완 on 2022/07/24.
+//
+
+import UIKit
+
+class OnboadingCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func configure(_ message: OnboardingMessage) {
+        thumbnailImageView.image = UIImage(named: message.imageName)
+        titleLabel.text = message.title
+        descriptionLabel.text = message.description
+    }
+    
+}
